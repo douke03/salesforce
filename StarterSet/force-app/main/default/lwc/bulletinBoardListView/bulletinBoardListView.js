291daf79ce1090f74696b887b7b1581c1e053c84
Shoki Nakano <nanaedashokudai@gmail.com>
2020-05-08 14:56:22 +0900

    UPDATE

--- StarterSet/force-app/main/default/lwc/bulletinBoardListView/bulletinBoardListView.js
+++ StarterSet/force-app/main/default/lwc/bulletinBoardListView/bulletinBoardListView.js
@@ -2,22 +2,18 @@ import { LightningElement, track, wire } from 'lwc';
 import getBulletinBoardList from '@salesforce/apex/BulletinBoardController.getImportantBulletinBoardList';
 
 export default class BulletinBoardListView extends LightningElement {
-    @track openmodel = false;
+    @track isOpenmodel = false;
     @track subject = '';
     @track bodyText = '';
     @track recordUrl = '';
     @wire(getBulletinBoardList) bulletinBoards;
     openModal(event) {
-        event.preventDefault();
-        event.stopPropagation();
         this.subject = event.currentTarget.dataset.subject;
         this.bodyText = event.currentTarget.dataset.bodyText;
         this.recordUrl = window.location.origin + '/' + event.currentTarget.dataset.id;
-        this.openmodel = true;
+        this.isOpenmodel = true;
     }
-    closeModal(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        this.openmodel = false;
+    closeModal() {
+        this.isOpenmodel = false;
     }
 }
--- StarterSet/force-app/main/default/lwc/modalInfoMedium/modalInfoMedium.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class ModalInfoMedium extends LightningElement {
-    @api subject;
-    @api bodyText;
-    @api recordUrl;
-    connectedCallback() {
-    }
-}
